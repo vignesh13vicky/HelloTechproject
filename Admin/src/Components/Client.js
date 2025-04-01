db5253933669d41a8f1229622c453d33bfb3702a
vignesh13vicky <vigneshvicky13052003@gmail.com>
2025-04-01 15:49:50 +0530

    after protected routes

--- Admin/src/Components/Client.js
+++ Admin/src/Components/Client.js
@@ -2,4 +2,4 @@ import axios from "axios";
 
 // export const Client = axios.create({baseURL:"http://localhost:7777" ,withCredentials: true,});
 
-export const Client = axios.create({baseURL:"https://hello-techproject-server.vercel.app" });
+export const Client = axios.create({ baseURL: "https://hello-techproject-server.vercel.app/" });
